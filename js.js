6437e4aa8063ebc0b519558ad3623ea83d1429f0
sebas castellanos <scs21098@gmail.com>
2023-08-01 19:05:34 -0500

    mecanica basica

---
+++ js.js
@@ -0,0 +1,21 @@
+//* function find_max(nums){
+   //*  let max_num = Number.NEGATIVE_INFINITY;
+   //*  for (let num of nums){
+    //*     if (num > max_num){
+    //*         max_num = num;
+
+  //*       }
+  //*   }
+ //*    return max_num;
+//* }
+//* console.log(find_max);
+//* ("volvo") *//
+
+let kia = "23";
+alert(kia);
+let cardeposito = prompt("carro prompt");
+alert("car"+ cardeposito)
+ let vari = 23;
+ console.log(vari);
+ let calculation = 23 + 77;
+ 
